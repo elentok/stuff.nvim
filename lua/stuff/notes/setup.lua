28fdbc76f650bb0ce02f15ce6a69de80f08db241
David Elentok <3david@gmail.com>
2025-03-27 22:09:40 +0200

    notes stuff

--- lua/stuff/notes/setup.lua
+++ lua/stuff/notes/setup.lua
@@ -2,6 +2,8 @@ local function setup()
   vim.keymap.set("n", "<leader>jw", function()
     require("stuff.notes.weekly").jump_to_weekly()
   end, { desc = "Jump to weekly note" })
+
+  vim.keymap.set("i", "<c-x>t", "- [ ]")
 end
 
 return setup
