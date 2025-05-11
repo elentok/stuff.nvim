7cbb0a222bf6b6375217525c68285f305e238d62
David Elentok <3david@gmail.com>
2025-05-11 09:49:03 +0300

    typescript: add TsServerLog command

--- lua/stuff/typescript/setup.lua
+++ lua/stuff/typescript/setup.lua
@@ -12,6 +12,13 @@ local function setup()
     function() require("stuff.typescript.toggle-async")() end,
     { desc = "Toggle async on closest function" }
   )
+
+  vim.api.nvim_create_user_command("TsServerLog", function()
+    local client = require("stuff.util.lsp").get_buf_client("vtsls")
+    if client ~= nil then
+      client:exec_cmd({ command = "typescript.openTsServerLog", title = "Open TS Server Log" })
+    end
+  end, {})
 end
 
 return setup
--- lua/stuff/util/lsp.lua
+++ lua/stuff/util/lsp.lua
@@ -9,4 +9,16 @@ local function action(name)
   })
 end
 
-return { action = action }
+--- Get
+---@param name string
+---@return vim.lsp.Client|nil
+local function get_buf_client(name)
+  local clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf(), name = name })
+  if #clients == 0 then
+    vim.notify("No LSP client named " .. name, vim.log.levels.WARN)
+    return nil
+  end
+  return clients[1]
+end
+
+return { action = action, get_buf_client = get_buf_client }
