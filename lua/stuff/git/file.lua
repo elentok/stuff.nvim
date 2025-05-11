8726d1d04b9b0a74b62482ade3b390bb3b9d1842
David Elentok <3david@gmail.com>
2025-05-11 09:52:53 +0300

    git: support actions when not in project root

--- lua/stuff/git/file.lua
+++ lua/stuff/git/file.lua
@@ -2,24 +2,24 @@ local function reset_changes()
   local ui = require("stuff.util.ui")
   if ui.confirm("Reset changes in current file?") then
     vim.cmd("silent edit!")
-    vim.fn.system("git checkout -- " .. vim.fn.shellescape(vim.fn.expand("%")))
+    vim.fn.system("git checkout -- " .. vim.fn.shellescape(vim.fn.expand("%:p")))
     vim.cmd("silent edit")
   end
 end
 
 local function add_with_patch()
   local run = require("stuff.git.run")
-  run.in_terminal({ "add", "-p", vim.fn.expand("%") })
+  run.in_terminal({ "add", "-p", vim.fn.expand("%:p") })
 end
 
 local function checkout_with_patch()
   local run = require("stuff.git.run")
-  run.in_terminal({ "checkout", "-p", vim.fn.expand("%") })
+  run.in_terminal({ "checkout", "-p", vim.fn.expand("%:p") })
 end
 
 local function write_and_stage()
   vim.cmd("write")
-  vim.system({ "git", "add", vim.fn.expand("%") }):wait()
+  vim.system({ "git", "add", vim.fn.expand("%:p") }):wait()
   print("Staged file")
 end
 
