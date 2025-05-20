3643e32e4ef86378ceaf6be88e4f656b4251a6d7
David Elentok <3david@gmail.com>
2025-05-20 17:51:54 +0300

    format notes

--- lua/stuff/notes/setup.lua
+++ lua/stuff/notes/setup.lua
@@ -1,7 +1,10 @@
 local function setup()
-  vim.keymap.set("n", "<leader>jw", function()
-    require("stuff.notes.weekly").jump_to_weekly()
-  end, { desc = "Jump to weekly note" })
+  vim.keymap.set(
+    "n",
+    "<leader>jw",
+    function() require("stuff.notes.weekly").jump_to_weekly() end,
+    { desc = "Jump to weekly note" }
+  )
 
   vim.keymap.set("i", "<c-x>t", "- [ ]")
   vim.cmd("abbr tsk - [ ]")
